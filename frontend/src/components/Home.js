55fdf4d5ffab38ad2ccfe9c52d4416a625c51c38
rameeznaufal <rameeznaufal12@gmail.com>
2021-09-25 13:55:01 +0400

    Signup & login

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = ({user}) => {
     return (
         <div>
-            Home Page of {user.name}
+            Home Page of {user? user.name:null }
         </div>
     );
 };
